1237a7dea397eb5d77285d2871c6f3cf92d3a1d1
learena <67345676+learena@users.noreply.github.com>
2025-01-09 10:59:45 +0100

    Update chatbot_streamlit_combined.py

--- chatbot_streamlit_combined.py
+++ chatbot_streamlit_combined.py
@@ -69,7 +69,7 @@ def display_chatbot_page():
                 llm_model = st.text_input("LLM model", value="tiiuae/falcon-7b-instruct")
 
             with row_1[2]:
-                instruct_embeddings = st.text_input("Instruct Embeddings", value=""hkunlp/instructor-xl" ")
+                instruct_embeddings = st.text_input("Instruct Embeddings", value=""hkunlp/instructor-xl")
 
             row_2 = st.columns(3)
             with row_2[0]:
